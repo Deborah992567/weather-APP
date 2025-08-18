abfc037d3a3b206007809a2d11fc5d232a1e47c7
uchechiiwuh <debbieofficial214@gmail.com>
2025-08-18 14:36:10 +0200

    added some changes to the database.py

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-URL_DATABASE = 'postgresql://postgres:Echolacc11@localhost:5432/weatherapp'
+URL_DATABASE = 'postgresql://weatherapp_q104_user:9jQeAZMgaU1DqWGDemBLjdRS9AV2fXz3@localhost:5432/weatherapp_q104'
 
 engine = create_engine(URL_DATABASE)
 
